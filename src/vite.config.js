22eb649b186a4adb0a9dd2b3da4bc16c912bad92
jemila2 <abubakarjemiletu08@gmail.com>
2025-08-27 02:52:49 +0100

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-
+  base: '/lsrepo/',
    server: {
     host: '0.0.0.0',
     port: 5173,
